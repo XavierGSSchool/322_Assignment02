0402eb1fda85697273f6b1f4d17e1743139763aa
Xavier Guinness Stout <xstout@mtech.edu>
2025-09-15 12:21:59 -0600

    Added all declarations for math functions in math_ops.h

---
+++ include/math_ops.h
@@ -0,0 +1,15 @@
+#ifndef MATH_OPS_H
+#define MATH_OPS_H
+
+// --------- FUNCTION DECLARATIONS ------------
+
+float square(float x);
+float cube(float x);
+float pow(float base, int exponent);
+float multiply(float x, float y);
+float add(float x, float y);
+float sub(float a, float b);
+
+
+/* End of UNITY_INTERNALS_H */
+#endif 
